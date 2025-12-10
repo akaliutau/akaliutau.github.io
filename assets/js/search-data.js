550fd984c49c573fc66f3594144c700a0fbb6294
akaliutau <aliaksei.kaliutau@gmail.com>
2025-12-10 20:22:27 +0000

    Deploying to gh-pages from @ akaliutau/akaliutau.github.io@81d4311d33b4f7fe20d98f79562b4b21c2cc0a52 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,18 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "post-a-post-to-test-the-posting",
-        
-          title: "a post to test the posting",
-        
-        description: "description",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2025/formatting-and-links/";
-          
-        },
-      },{id: "post-mvc-2024-highlights-and-impressions",
+        },{id: "post-mvc-2024-highlights-and-impressions",
         
           title: 'MVC 2024: highlights and impressions <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
