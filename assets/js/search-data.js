7e7345578d96fc8c1fc58f6a57b4f747eca6fa60
akaliutau <aliaksei.kaliutau@gmail.com>
2026-09-21 11:45:55 +0000

    Deploying to gh-pages from @ akaliutau/akaliutau.github.io@79eb3b85628d0b546da6ffa87c916494200b6cdc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -163,6 +163,11 @@ ninja.data = [{
           description: "Adaptive AI ANC system",
           section: "Projects",handler: () => {
               window.location.href = "/projects/quietplace/";
+            },},{id: "projects-stillemap",
+          title: 'StilleMap',
+          description: "Urban noise digital twin.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/stillemap/";
             },},{id: "projects-square-tree-or-how-to-pack-200-x-mas-trees",
           title: 'Square Tree, or How To Pack 200 X-mas Trees',
           description: "Distributed Geometric Packing via Crystallographic Lattices.",
